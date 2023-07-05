758792b8d5e79ad5a2a312750abb8cbb8d179819
ThomasLe <88428250+ThomasLe-dev@users.noreply.github.com>
2023-07-05 08:50:58 +0700

    push code

--- FE/nhom4-fc/src/layouts/TheHeader.vue
+++ FE/nhom4-fc/src/layouts/TheHeader.vue
@@ -3,23 +3,10 @@
     <div class="header__left">
       <div class="header__menu"></div>
       <div class="header__logo">
-        <a class="logo__nav" href="#"> </a>
-        <a class="logo__title" href="#">MISA CeGov</a>
+        <a class="logo__title" href="#">MISA QA</a>
       </div>
     </div>
     <div class="header__right">
-      <div class="header__action">
-        <span class="m-icon notify"></span>
-      </div>
-      <div class="header__action">
-        <span class="m-icon help"></span>
-      </div>
-      <div class="header__action">
-        <span class="m-icon document"></span>
-      </div>
-      <div class="header__action">
-        <span class="m-icon setting"></span>
-      </div>
       <div class="header__avatar">
         <img src="../assets/img/getavatar.png" alt="avatar" />
       </div>
